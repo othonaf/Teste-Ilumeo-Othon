4f81a2a052d0f948be89287387420c7028c33383
othonaf <othon.ab@hotmail.com>
2025-01-13 10:16:36 -0300

    Atualiza arquivo Readme.md

--- backend-pontos/src/main.ts
+++ backend-pontos/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
